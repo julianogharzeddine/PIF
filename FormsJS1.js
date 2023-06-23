1af2b0031cde04bd7cbb485c0ed22b86cd583fa5
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-06-23 12:57:57 +0300

    Update and rename FormsJS.js to FormsJS1.js

--- FormsJS1.js
+++ FormsJS1.js
@@ -7,8 +7,10 @@ function changeLang(){
     var lang = localStorage.getItem("selected_language")
     if (lang == "ar-SA") {
         $(".row:has(div[name*=Card])").css("flex-direction", "row-reverse");
+              $('.file-info-cell').find('div.file-watermark').text("إضغط هنا لتحميل ملف")
     } else {
         $(".row:has(div[name*=Card])").css("flex-direction", "row");
+        $('.file-info-cell').find('div.file-watermark').text("Click here to attach a file")
     }
     }
     function waitForWrapperRenderForLang() {
